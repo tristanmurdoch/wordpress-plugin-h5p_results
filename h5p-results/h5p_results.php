a4be53a6d29ab54387c50ef61135ac4841808e20
Tristan Mackay <t.mackay@murdoch.edu.au>
2017-10-04 10:40:53 +0800

    adding test to display userid 3

--- h5p-results/h5p_results.php
+++ h5p-results/h5p_results.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: H5P Results
+ * Plugin Name: H5P Shortcodes
  * Description: Adds custom shortcodes for H5P results.
  * Author: Tristan Mackay
  * Version: 1.0
- * Text Domain: h5p-results
+ * Text Domain: h5p-shortcodes
  *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
- * @author    Tristan Mackay
+ * @author    TMAC
  * @copyright Copyright (c) 2017
  *
  */
@@ -18,124 +18,135 @@
 defined( 'ABSPATH' ) or exit;
 
 function h5p_results_func($atts){
-	
-	global $wpdb;
+    
+    global $wpdb;
 
- 	$atts = shortcode_atts( array(
-        	'display' => 'list',
-		'tablename' => 'H5P Results',
-		'userid' => 'none',
-    	), $atts );
+    $atts = shortcode_atts( array(
+            'display' => 'list',
+        'tablename' => 'H5P Results',   
+        'userid' => 'none',
+        ), $atts );
 
 
-	$current_user = wp_get_current_user();
-	   	if($atts['userid'] !== 'none')
-		  $current_user->ID = $atts['userid'];
+    $current_user = wp_get_current_user();
+        if($atts['userid'] !== 'none')
+          $current_user->ID = $atts['userid'];
 
-	$results = $wpdb->get_results( "SELECT id, slug FROM `wp_h5p_contents`;" );
+    $results = $wpdb->get_results( "SELECT id, slug FROM `wp_h5p_contents`;" );
 
-	$sql_string = 'SELECT ';
+    $sql_string = 'SELECT ';
 
-   	if($atts['userid'] == 'all'){
-		$sql_string = "SELECT u.display_name, ";
-		$atts['display'] = 'row';
-   	}
+    if($atts['userid'] == 'all'){
+        $sql_string = "SELECT u.display_name, ";
+        $atts['display'] = 'row';
+    }
 
-	foreach ($results as $result){
-		$sql_string .=  "IFNULL(CONCAT(r".$result->id.".score , '/', r".$result->id.".max_score),'N/A') as '".$result->slug."', ";
-	}
+    foreach ($results as $result){
+        $sql_string .=  "IFNULL(CONCAT(r".$result->id.".score , '/', r".$result->id.".max_score),'N/A') as '".$result->slug."', ";
+    }
 
-	$sql_string = substr($sql_string, 0, -2);
+    $sql_string = substr($sql_string, 0, -2);
 
-	$sql_string .= " FROM `wp_users` as u ";
+    $sql_string .= " FROM `wp_users` as u ";
 
-	foreach ($results as $result){
+    foreach ($results as $result){
 
-		$sql_string .= "left join `wp_h5p_results` as r".$result->id." on r".$result->id.".user_id = u.id and r".$result->id.".content_id = ".$result->id." ";
-	}
+        $sql_string .= "left join `wp_h5p_results` as r".$result->id." on r".$result->id.".user_id = u.id and r".$result->id.".content_id = ".$result->id." ";
+    }
 
-	if($atts['userid'] !== 'all')
-	   $sql_string .= "WHERE u.id =".$current_user->ID ;
-	
-	$results = $wpdb->get_results($sql_string);
+    if($atts['userid'] !== 'all')
+       $sql_string .= "WHERE u.id =".$current_user->ID ;
 
-	if($atts['display'] == 'row'){
+// print_r($sql_string);
+    
+    $results = $wpdb->get_results($sql_string);
 
-		$html = "<table border='1'>".
-				'<table class="h5p-results">
-				<thead>
-				<tr>';
+// var_dump($results);
 
-		foreach ($results as $result){
-			$result = (array)$result;
-				$result_keys = array_keys($result); // get keys
-		}
+    if($atts['display'] == 'row'){
 
-		foreach ($result_keys as $result_key){
-			$html .= '<th>'.$result_key.'</th>';
-		}
+        $html = "<table border='1'>".
+                '<table class="h5p-results">
+                <thead>
+                <tr>';
 
-		$html .= '</tr></thead>';
+        foreach ($results as $result){
+            $result = (array)$result;
+                $result_keys = array_keys($result); // get keys
+        }
 
-		foreach ($results as $row){
-			$html .= "<tr>";
-		
-			foreach ($row as $r){
-				$html .= "<td>" . $r . "</td>";
-			}
-		$html .= "</tr>";
-	//close off table mn
-		}
-	$html .= "</tbody></table>";
+        foreach ($result_keys as $result_key){
+            $html .= '<th>'.$result_key.'</th>';
+        }
 
-	}
+        $html .= '</tr></thead>';
 
-	if($atts['display'] == 'list'){
+        foreach ($results as $row){
+            $html .= "<tr>";
+        
+            foreach ($row as $r){
+                $html .= "<td>" . $r . "</td>";
+            }
+        $html .= "</tr>";
+    //close off table mn
+        }
+    $html .= "</tbody></table>";
 
-		$sql = "SELECT c.title, c.slug, r.score, r.max_score, ".
-		"r.opened, r.finished FROM `wp_h5p_results` r LEFT JOIN ".
-		"`wp_h5p_contents` AS c ON c.id = r.content_id WHERE r.user_id = "
-		.$current_user->ID;
+    }
 
-		$results = $wpdb->get_results($sql);
+    if($atts['display'] == 'list'){
 
-		$html = '<table>
-				<caption>'.$atts['tablename'].'</caption>
-				<thead>
-				<tr>
-				<th scope="col">Title</th>
-				<th scope="col">Result</th>
-				<th scope="col">Result %</th>
-				<th scope="col">Completion date</th>
-				<th scope="col">Time taken</th>
-				</tr>
-				</thead>
-				<tbody>';
+        $sql = "SELECT c.title, c.slug, r.score, r.max_score, ".
+        "r.opened, r.finished FROM `wp_h5p_results` r LEFT JOIN ".
+        "`wp_h5p_contents` AS c ON c.id = r.content_id WHERE r.user_id = "
+        .$current_user->ID;
 
-		if(count($results) >= 1){
+        $results = $wpdb->get_results($sql);
 
-			foreach($results as $result){
-				// work out time taken 
-				$duration = (string)$result->finished - (string)$result->opened;		
-				$minutes = floor($duration / 60);
-				$seconds = $duration % 60;
-				// convert score to %
-				$score_percent = (string)$result->score / (string)$result->max_score * 100;
+        print_r($sql);
+        var_dump($results);
 
-				$result->finished = date('m/d/Y', $result->finished);
+        $sql = "SELECT * FROM wp_h5p_results where user_id = 3";
+        $resultstest = $wpdb->get_results($sql);
+        var_dump($resultstest);
 
-				$html .= '<tr><td>'.$result->title.'</td><td>'.
-				$result->score.'/'.$result->max_score.'</td><td>'.
-				substr($score_percent,0,5).'</td><td>'.
-				$result->finished.'</td><td>'.
-				$minutes.':'.$seconds.'</td></tr>';
-				}
-		}else{
-		$html .= '<tr><td colspan="5">No results</td></tr>';
-		}
-	$html .= '</tbody></table>';
+        $html = '<table>
+                <caption>'.$atts['tablename'].'</caption>
+                <thead>
+                <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Result</th>
+                <th scope="col">Result %</th>
+                <th scope="col">Completion date</th>
+                <th scope="col">Time taken</th>
+                </tr>
+                </thead>
+                <tbody>';
 
-	}
+        if(count($results) >= 1){
+
+            foreach($results as $result){
+                // work out time taken 
+                $duration = (string)$result->finished - (string)$result->opened;        
+                $minutes = floor($duration / 60);
+                $seconds = $duration % 60;
+                // convert score to %
+                $score_percent = (string)$result->score / (string)$result->max_score * 100;
+
+                $result->finished = date('m/d/Y', $result->finished);
+
+                $html .= '<tr><td>'.$result->title.'</td><td>'.
+                $result->score.'/'.$result->max_score.'</td><td>'.
+                substr($score_percent,0,5).'</td><td>'.
+                $result->finished.'</td><td>'.
+                $minutes.':'.$seconds.'</td></tr>';
+                }
+        }else{
+        $html .= '<tr><td colspan="5">No results</td></tr>';
+        }
+    $html .= '</tbody></table>';
+
+    }
 unset($atts);
 unset($current_user);
 return($html);
